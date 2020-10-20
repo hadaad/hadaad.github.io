5a6d18b13380c007ab2ea9ea951c333931bb4972
Liam Johnstone <liam@bisonweb.ca>
2020-10-19 22:13:05 -0600

    deploy with rss

---
+++ assets/js/13.3718ea2d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{360:function(t,n,e){"use strict";e.r(n);var s=e(40),i=Object(s.a)({},(function(){var t=this.$createElement,n=this._self._c||t;return n("div",[n("h1",[this._v(this._s())])])}),[],!1,null,null,null);n.default=i.exports}}]);
